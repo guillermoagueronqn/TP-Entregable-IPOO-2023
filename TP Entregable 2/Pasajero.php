332f9344474d372f79677f33ea6bd4219f8d5e36
guillermoagueronqn <guillermoagueronqn@gmail.com>
2023-04-17 19:03:46 -0300

    Agrego el archivo que contiene la clase Pasajero

---
+++ TP Entregable 2/Pasajero.php
@@ -0,0 +1,99 @@
+<?php
+/**Modificar la clase Viaje para que ahora los pasajeros sean un objeto que tenga los atributos 
+nombre, apellido, numero de documento y teléfono.*/
+
+class Pasajero {
+    private $nombre;
+    private $apellido;
+    private $nroDocumento;
+    private $telefono;
+
+    /**
+     * METODO constructor de la clase pasajero
+     * @param string
+     * @param string
+     * @param int
+     * @param int
+     */
+    public function __construct($pNombre, $pApellido, $pNroDocumento, $pTelefono){
+        $this -> nombre = $pNombre;
+        $this -> apellido = $pApellido;
+        $this -> nroDocumento = $pNroDocumento;
+        $this -> telefono = $pTelefono;
+    }
+
+    /**
+     * METODO para obtener el nombre del pasajero
+     * @return string
+     */
+    public function getNombre (){
+        return $this -> nombre;
+    }
+
+    /**
+     * METODO para obtener el apellido del pasajero
+     * @return string
+     */
+    public function getApellido (){
+        return $this -> apellido;
+    }
+
+    /**
+     * METODO para obtener el numero de documento del pasajero
+     * @return int
+     */
+    public function getNroDocumento (){
+        return $this -> nroDocumento;
+    }
+
+    /**
+     * METODO para obtener el telefono del pasajero
+     * @return int
+     */
+    public function getTelefono (){
+        return $this -> telefono;
+    }
+
+    /**
+     * METODO para setear el nombre del pasajero
+     * @param string
+     */
+    public function setNombre ($nuevoNombre){
+        $this -> nombre = $nuevoNombre;
+    }
+
+    /**
+     * METODO para setear el apellido del pasajero
+     * @param string
+     */
+    public function setApellido ($nuevoApellido){
+        $this -> apellido = $nuevoApellido;
+    }
+
+    /**
+     * METODO para setear el numero de documento del pasajero
+     * @param int
+     */
+    public function setNroDocumento ($nuevoNroDocumento){
+        $this -> nroDocumento = $nuevoNroDocumento;
+    }
+
+    /**
+     * METODO para setear el telefono del pasajero
+     * @param int
+     */
+    public function setTelefono ($nuevoTelefono){
+        $this -> telefono = $nuevoTelefono;
+    }
+
+    /**
+     * METODO que retorna un string con toda la información del pasajero
+     */
+    public function __toString(){
+        return "INFORMACIÓN DEL PASAJERO:\n" . 
+        "NOMBRE: " . $this->getNombre() . "\n" . 
+        "APELLIDO: " . $this->getApellido() . "\n" . 
+        "NUMERO DE DOCUMENTO: " . $this->getNroDocumento() . "\n" . 
+        "TELEFONO: " . $this->getTelefono() . "\n";
+    }
+}
